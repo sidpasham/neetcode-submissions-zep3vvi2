79ab8d05e1e3e0cea9d3764d4a3b0bed3955daca
Siddhanth Pasham <44481936+sidpasham@users.noreply.github.com>
2026-04-20 18:49:07 -0500

    Add: three-integer-sum - submission-3

---
+++ Data Structures & Algorithms/three-integer-sum/submission-3.cs
@@ -0,0 +1,42 @@
+public class Solution {
+    public List<List<int>> ThreeSum(int[] nums) {
+        // edge cases
+
+        var result = new List<List<int>>();
+        Array.Sort(nums);
+
+        for(int i = 0; i<nums.Length; i++) {
+            // edge case
+            if (nums[i] > 0) {
+                break;
+            }
+            // edge case
+            if(i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+            int l = i + 1;
+            int r = nums.Length -1;
+
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if(sum < 0) {
+                    l++;
+                } else if (sum > 0) {
+                    r--;
+                } else {
+                    result.Add(new List<int>() {nums[i], nums[l], nums[r]});
+                    l++;
+                    r--;
+
+                    // edge case
+                    while(l < r && nums[l] == nums[l -1]) {
+                        l++;
+                    }
+                }
+                
+            }
+        }
+
+        return result;        
+    }
+}
