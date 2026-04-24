a89773c081f88e6a1dbacdcde27a51baaecbfea2
Siddhanth Pasham <44481936+sidpasham@users.noreply.github.com>
2026-04-24 00:12:32 -0500

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.cs
@@ -0,0 +1,33 @@
+public class Solution {
+    public int CharacterReplacement(string s, int k) {
+        // edge cases
+        int l = 0;
+        int r = 0;
+        var count = new Dictionary<char, int>();
+        int maxf = 0;
+        int longest = 0;
+        while(r < s.Length) {
+            if(count.ContainsKey(s[r])) {
+                count[s[r]]++;
+            } else {
+                count.Add(s[r], 1);
+            }
+            maxf = Math.Max(maxf, count[s[r]]);
+
+            // sliding window condition
+            int windowLength = r - l + 1;
+            if(windowLength - maxf > k) {
+                count[s[l]]--;
+                l++;                
+            }
+            
+            // calculate length
+            int cur = r - l + 1;
+            if(cur > longest) {
+                longest = cur;
+            }
+            r++;
+        }
+        return longest;
+    }
+}
