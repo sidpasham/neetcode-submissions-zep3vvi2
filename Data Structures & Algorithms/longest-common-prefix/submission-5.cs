93e8e34df7bad3f265813441396f6d47997e11cc
Siddhanth Pasham <44481936+sidpasham@users.noreply.github.com>
2026-04-29 09:08:44 -0500

    Add: longest-common-prefix - submission-5

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-5.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public string LongestCommonPrefix(string[] strs) {
+        // TODO edgecases
+
+        var first = strs[0];
+        string prefix = "";
+
+        for(int i = 0; i< first.Length; i++) {
+            foreach(var str in strs) {
+                if(i == str.Length || first[i] != str[i]) {
+                    return prefix;
+                }
+            }
+            prefix += first[i];
+        }
+
+        return prefix;
+        
+    }
+}
